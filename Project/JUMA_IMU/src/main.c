0cb967e27e4387f17cffb704b9fa59856de01767
Inc <1143547963@qq.com>
2016-03-21 14:28:07 +0800

    add project off IMU

---
+++ Project/JUMA_IMU/src/main.c
@@ -0,0 +1,272 @@
+/**
+  ******************************************************************************
+  * @file    Templates/Src/main.c
+  * @author  MCD Application Team
+  * @version V1.2.3
+  * @date    29-January-2016
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT(c) 2016 STMicroelectronics</center></h2>
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "usb_device.h"
+#include "usbd_cdc_if.h"
+#include "I2CSoft.h"
+#include "HTS221.h"
+#include "lsm6ds3.h"
+/** @addtogroup STM32F4xx_HAL_Examples
+  * @{
+  */
+
+/** @addtogroup Templates
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+extern uint32_t UserTxBufPtrIn;
+extern uint32_t UserTxBufPtrOut;
+extern uint8_t UserTxBufferFS[];
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+static void SystemClock_Config(void);
+
+
+/* Private functions ---------------------------------------------------------*/
+/* USER CODE BEGIN 4 */
+uint8_t rxBuf[100] ={0};
+void Main_loop(void)
+{
+    uint32_t buffsize;
+
+    if(UserTxBufPtrOut != UserTxBufPtrIn)
+    {
+        if(UserTxBufPtrOut > UserTxBufPtrIn) /* Rollback */
+        {
+            buffsize= UserTxBufPtrIn;
+        }
+        else
+        {
+            buffsize = UserTxBufPtrIn - UserTxBufPtrOut;
+        }
+        UserTxBufPtrOut += buffsize;
+        memcpy(rxBuf,UserTxBufferFS+UserTxBufPtrOut,buffsize);
+        CDC_Transmit_FS(rxBuf,buffsize);
+        //USBD_CDC_ReceivePacket(&hUsbDeviceFS);
+    }
+}
+
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+uint8_t HELLO0[] = "----->/**********************/----->\r\n";
+uint8_t HELLO1[] = "<----->/**********************/<----->\r\n";
+uint8_t HELLO2[] = "<----->/**********************/-----<\r\n";
+
+
+int32_t Data[3] = {0};
+int main(void)
+{
+    uint8_t id;
+    float Hum;
+    float Temp;
+    
+    IMU_6AXES_InitTypeDef InitStructure;
+    
+    /* Configure sensor */
+    InitStructure.G_FullScale      = 2000.0f; /* 2000DPS */
+    InitStructure.G_OutputDataRate = 200.0f;  /* 104HZ */
+    InitStructure.G_X_Axis         = 1;       /* Enable */
+    InitStructure.G_Y_Axis         = 1;       /* Enable */
+    InitStructure.G_Z_Axis         = 1;       /* Enable */
+
+    InitStructure.X_FullScale      = 2.0f;    /* 2G */
+    InitStructure.X_OutputDataRate = 200.0f;  /* 104HZ */
+    InitStructure.X_X_Axis         = 1;       /* Enable */
+    InitStructure.X_Y_Axis         = 1;       /* Enable */
+    InitStructure.X_Z_Axis         = 1;       /* Enable */
+    /* STM32F4xx HAL library initialization:
+       - Configure the Flash prefetch, Flash preread and Buffer caches
+       - Systick timer is configured by default as source of time base, but user
+             can eventually implement his proper time base source (a general purpose
+             timer for example or other time source), keeping in mind that Time base
+             duration should be kept 1ms since PPP_TIMEOUT_VALUEs are defined and
+             handled in milliseconds basis.
+       - Low Level Initialization
+     */
+    HAL_Init();
+
+    /* Configure the System clock to have a frequency of 84 MHz */
+    SystemClock_Config();
+
+
+    /* Add your application code here
+     */
+    //UsbDeviceInit();
+    HAL_Delay(3000);
+    
+    i2c_soft.dev_on();
+    
+    while(IMU_6AXES_OK != LSM6DS3Drv.Init(&InitStructure)){
+        BSP_LED_Toggle(LED1);
+        HAL_Delay(200);
+    }
+    
+    if(IMU_6AXES_OK == LSM6DS3Drv.Read_XG_ID(&id)){
+        while( id != I_AM_LSM6DS3_XG){
+            BSP_LED_Toggle(LED1);
+            HAL_Delay(500);
+        }
+    }
+
+    while(HUM_TEMP_SUCCESS != hts221.dev_on(&i2c_soft)){
+        BSP_LED_Toggle(LED1);
+        HAL_Delay(200);
+    }
+    if(HUM_TEMP_SUCCESS == hts221.read_id(&id)){
+    }
+
+    while( id != I_AM_HTS221){
+        hts221.read_id(&id);
+        BSP_LED_Toggle(LED1);
+        HAL_Delay(500);
+    }
+    #if 0
+    while(CDC_Transmit_FS(HELLO0,sizeof(HELLO0)));
+    while(CDC_Transmit_FS(HELLO1,sizeof(HELLO1)));
+    while(CDC_Transmit_FS(HELLO2,sizeof(HELLO2)));
+    #endif
+    BSP_LED_Init(LED1);
+
+    /* Infinite loop */
+    while (1){
+        BSP_LED_Toggle(LED1);
+        //i2c_multi_read(0xBE,0x0F,&id, 1);
+        //i2c_soft.multi_read_byte(0xBE,0x0F,&id, 1);
+        //while(CDC_Transmit_FS(&id,sizeof(id)));
+        //Main_loop();
+        hts221.get_humidity(&Hum);
+        hts221.get_temperature(&Temp);
+        LSM6DS3Drv.Get_X_Axes(Data);
+        HAL_Delay(1000);
+    }
+}
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow :
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 84000000
+  *            HCLK(Hz)                       = 84000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 2
+  *            APB2 Prescaler                 = 1
+  *            HSE Frequency(Hz)              = 8000000
+  *            PLL_M                          = 8
+  *            PLL_N                          = 336
+  *            PLL_P                          = 4
+  *            PLL_Q                          = 7
+  *            VDD(V)                         = 3.3
+  *            Main regulator output voltage  = Scale2 mode
+  *            Flash Latency(WS)              = 2
+  * @param  None
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+
+    RCC_OscInitTypeDef RCC_OscInitStruct;
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+    __PWR_CLK_ENABLE();
+
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 16;
+    RCC_OscInitStruct.PLL.PLLN = 336;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+    RCC_OscInitStruct.PLL.PLLQ = 7;
+    HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_PCLK1;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
+
+    HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+    /* SysTick_IRQn interrupt configuration */
+    HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+
+}
+
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
